d5c4409db3ca333efcb445aba146c80701d37c51
James Laverack <james@jameslaverack.com>
2017-12-20 11:45:02 +1100

    Remove autostart

--- .autostart/README.md
+++
@@ -1,1 +0,0 @@
-Files in here should be launched via autostart. Ideally not run as part of `.zshrc`.
--- .autostart/emacs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# This should be invoked on startup, usually as a window manager autostart. It
-# probably shouldn't be placed in .zshrc as you don't really want to do this
-# on every shell start
-emacs --daemon
