5b6360abbd249d30df57f6a5201dd7de6e090782
James Laverack <james@jameslaverack.com>
2019-02-13 11:15:16 +0000

    Change theme to `doom-vibrant`

--- .emacs.d/personal/theme.el
+++ .emacs.d/personal/theme.el
@@ -1,5 +1,5 @@
 (prelude-require-package 'doom-themes)
-(setq prelude-theme 'doom-molokai)
+(setq prelude-theme 'doom-vibrant)
 
 ;; Hack because this file is loaded after core/prelude-ui.el
 (load-theme prelude-theme t)
