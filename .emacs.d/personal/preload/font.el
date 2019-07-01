5ba3675a311d192e1de500bc108a0fef993ebfa0
James Laverack <james@jameslaverack.com>
2019-07-01 13:26:16 +0100

    Revert "Use variable font"
    This reverts commit 960ac33a5b2df6ebcc22d73349775796d4a52ac6.

--- .emacs.d/personal/preload/font.el
+++ .emacs.d/personal/preload/font.el
@@ -1,1 +1,1 @@
-(add-to-list 'default-frame-alist '(font . "Source Code Variable:pixelsize=21:foundry=ADBO:weight=normal:slant=normal:width=normal:spacing=100:scalable=true"))
+(add-to-list 'default-frame-alist '(font . "Source Code Pro semibold 18"))
