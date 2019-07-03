effb02173783d53f9cf9af7ea47c7f21b305c878
James Laverack <james@jameslaverack.com>
2019-07-03 10:02:09 +0100

    Add macOS configuration

---
+++ .emacs.d/personal/macos.el
@@ -0,0 +1,7 @@
+(if
+ (string-equal system-type "darwin")
+  (progn
+   (setq mac-option-key-is-meta nil)
+   (setq mac-command-key-is-meta t)
+   (setq mac-command-modifier 'meta)
+   (setq mac-option-modifier nil)))
